7cb55a407cddb74c4a9a2ec45365b1d65fd0f992
hyowon3434 <hyowon3434@naver.com>
2023-09-03 23:39:33 +0900

    fix: 데이터셋 삽입

--- server.py
+++ server.py
@@ -16,8 +16,8 @@ db = pymysql.connect(
 @app.route('/')
 def save_feed():
     crawler = instagram.InstaCrawling()
-    img_urls, video_urls = crawler.crawling()
-
+    img_urls, video_urls, img_date, video_date, img_writer, video_writer = crawler.crawling()
+    print(len(img_urls), len(video_urls), video_date, video_writer)
 
 
     cur = db.cursor()
@@ -29,12 +29,19 @@ def save_feed():
     for i in range(len(result)):
         result_url.append(result[i][3])
 
-    new_url = [x for x in video_urls if x not in result_url]
-
-
-    insert_sql = 'insert into insta_feed(created_At, file_url, writer) values (%s, %s, %s)'
-    cur.execute(insert_sql, ('2022-05-05', new_url, '작성자'))
+    # 영상 데이터 DB 추가
+    new_video_url = [x for x in video_urls if x not in result_url]
+    insert_video_sql = 'insert into insta_feed(created_At, file_url, writer) values (%s, %s, %s)'
+    cur.execute(insert_video_sql, (video_date, new_video_url, video_writer))
     db.commit()
+    print(2)
+
+    # 사진 데이터 DB 추가
+    new_img_url = [x for x in img_urls if x not in result_url]
+    insert_img_url = 'insert into insta_feed(created_At, file_url, writer) values (%s, %s, %s)'
+    cur.execute(insert_img_url, (img_writer, new_img_url, img_date))
+    db.commit()
+    print(len(img_writer), len(new_img_url), len(img_date))
     db.close()
 
 if __name__ == '__main__':
